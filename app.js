ea14938befc6b1c8781ae179bc815f7745ba592c
lana <lanabkim@gmail.com>
2020-12-15 23:31:38 -0800

    HTML rendering workding

--- app.js
+++ app.js
@@ -10,26 +10,162 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employeeList = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+const managerQ = [
+	{
+		type: "input",
+		message: "What is your manager's name?",
+		name: "name",
+	},
+	{
+		type: "input",
+		message: "What is your manager's ID?",
+		name: "id",
+	},
+	{
+		type: "input",
+		message: "What is your manager's Email?",
+		name: "email",
+	},
+	{
+		type: "input",
+		message: "What is your manager's Office Number?",
+		name: "officeNumber",
+	},
+];
+const engineerQ = [
+	{
+		type: "input",
+		message: "What is your engineer's name?",
+		name: "name",
+	},
+	{
+		type: "input",
+		message: "What is your engineer's ID?",
+		name: "id",
+	},
+	{
+		type: "input",
+		message: "What is your engineer's Email?",
+		name: "email",
+	},
+	{
+		type: "input",
+		message: "What is your engineers Github username?",
+		name: "github",
+	},
+];
+const internQ = [
+	{
+		type: "input",
+		message: "What is your intern's name?",
+		name: "name",
+	},
+	{
+		type: "input",
+		message: "What is your intern's ID?",
+		name: "id",
+	},
+	{
+		type: "input",
+		message: "What is your intern's Email?",
+		name: "email",
+	},
+	{
+		type: "input",
+		message: "What is your inter's School?",
+		name: "school",
+	},
+];
+const employeeTypeQ = [
+	{
+		type: "list",
+		message: "Which of team member would you like to add?",
+		name: "addingNew",
+		choices: [
+			"Engineer",
+			"Intern",
+			"I don't want to add any more team memebers",
+		],
+	},
+];
+console.log("please build your team");
+// ASK user for manager info
+function askManagerInfo() {
+	return inquirer.prompt(managerQ).then((managerData) => {
+		// console.log("Please build your new team!");
+		const newManager = new Manager(
+			managerData.name,
+			managerData.id,
+			managerData.email,
+			managerData.officeNumber
+		);
+		employeeList.push(newManager);
+		// console.log(employeeList);
+		askNextEmployeeType();
+	});
+}
+// ASK user for next employee type
+function askNextEmployeeType() {
+	return inquirer.prompt(employeeTypeQ).then((newChoiceData) => {
+		// if user selected Engineer
+		if (newChoiceData.addingNew === "Engineer") {
+			console.log("=====================================================");
+			console.log(newChoiceData);
+			askEngineerInfo();
+		}
+		// if user selected Intern
+		if (newChoiceData.addingNew === "Intern") {
+			console.log("=====================================================");
+			console.log(newChoiceData);
+			askInternInfo();
+		}
+		// Else, when user does not want to add more people
+		else {
+			renderHTML();
+		}
+	});
+}
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+// ASK user for engineer info
+function askEngineerInfo() {
+	return inquirer.prompt(engineerQ).then((engineerData) => {
+		const newEngineer = new Engineer(
+			engineerData.name,
+			engineerData.id,
+			engineerData.email,
+			engineerData.github
+		);
+		employeeList.push(newEngineer);
+		askNextEmployeeType();
+	});
+}
+// ASK user for intern info
+function askInternInfo() {
+	return inquirer.prompt(internQ).then((internData) => {
+		const newIntern = new Intern(
+			internData.name,
+			internData.id,
+			internData.email,
+			internData.school
+		);
+		employeeList.push(newIntern);
+		askNextEmployeeType();
+	});
+}
+function writeToFile(fileName, data) {
+	fs.writeFile(fileName, data, (err) => {
+		if (err) throw err;
+		// console.log(
+		// 	"You have New Team for your Project! Check your Output Folder! "
+		// );
+		// WHY this message  appear after choosing Engineer?
+	});
+}
+function renderHTML() {
+	const htmlContent = render(employeeList);
+	writeToFile(outputPath, htmlContent);
+}
+// starting
+askManagerInfo();
